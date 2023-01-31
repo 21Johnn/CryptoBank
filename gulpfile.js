35d85f43d2840d8dc4a804ccb810751f2a78e124
John <mktkamehouse@gmail.com>
2023-01-30 21:10:48 -0300

    encerramento do projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate');
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
@@ -14,8 +16,15 @@ function images(){
     .pipe(gulp.dest('./dist/images'));
 }
 
+function jsCompress(){
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(gulp.dest('./dist/scripts'));
+}
 
-exports.default = gulp.parallel(styles, images);
+
+exports.default = gulp.parallel(styles, images, jsCompress);
 exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles, jsCompress));
 }
---
+++ src/scripts/main.js
@@ -0,0 +1,128 @@
+// accordion
+const headers = document.querySelectorAll('.accordion__item__header');
+headers.forEach(header => {
+    header.addEventListener('click', function() {
+        console.log('Funcionou')
+    const content = this.nextElementSibling;
+    if(content.classList.contains("open")) {
+        content.style.height = "0";
+        content.classList.remove("open");
+        header.classList.remove("open");
+    } else {
+        headers.forEach(otherHeader => {
+        otherHeader.nextElementSibling.style.height = "0";
+        otherHeader.nextElementSibling.classList.remove("open");
+        otherHeader.classList.remove("open");
+    });
+        content.style.height = "auto";
+        content.classList.add("open");
+        header.classList.add("open");
+    }
+    });
+});
+
+
+// bitcoin
+
+var priceContainer = document.getElementById('value');
+var btn = document.getElementById('btn');
+let selectCurrency = document.getElementById('selected').value
+let close = document.getElementById('close');
+
+window.addEventListener('DOMContentLoaded', function(){
+    let url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+    let url2 = 'http://economia.awesomeapi.com.br/json/last/USD-BRL'
+
+    if (selectCurrency == 'USD'){
+        fetch(url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+
+            priceContainer.innerHTML = priceContainer.innerHTML = json.bpi.USD.symbol+json.bpi.USD.rate;
+        })
+    } else if (selectCurrency == 'BRL'){
+        fetch(url2)
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(json1){
+            console.log(json1)
+
+        fetch(url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            priceInUSD = json.bpi.USD.rate;
+            priceInBRL = "R$ " + (parseFloat(json1.USDBRL.bid) * parseFloat(priceInUSD)) + ",00";
+            console.log(priceInBRL)
+            
+            priceContainer.innerHTML = priceInBRL
+        })
+
+        })
+    }
+})
+
+
+btn.addEventListener('click', function(){
+
+    console.log('clicou')
+    let selectCurrency = document.getElementById('selected').value
+    let url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+    let url2 = 'http://economia.awesomeapi.com.br/json/last/USD-BRL'
+
+    if (selectCurrency == 'USD'){
+        fetch(url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+
+            priceContainer.innerHTML = json.bpi.USD.symbol+json.bpi.USD.rate;
+        })
+    } else if (selectCurrency == 'BRL'){
+        fetch(url2)
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(json1){
+            console.log(json1)
+
+        fetch(url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            priceInUSD = json.bpi.USD.rate;
+            cents = parseInt(Math.random() * 110)
+            priceInBRL = "R$ " + (parseFloat(json1.USDBRL.bid) * parseFloat(priceInUSD)).toFixed(3) + "," + cents;
+            
+            priceContainer.innerHTML = priceInBRL
+        })
+
+        })
+    }
+})
+
+close.addEventListener('click', function(){
+    let container = document.getElementById('bitcoin');
+
+    container.classList.add('hidden');
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+    let container = document.getElementById('bitcoin');
+
+    console.log("Carregou")
+    setTimeout(() => {
+        container.classList.remove('hidden');
+    }, 7000)
+})
+
+
+// change theme
+
+
